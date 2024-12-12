a182864b1e610983a7e8ac3c2eb72316105bc995
Simon Lupien <lupiensimon@hotmail.com>
2024-12-11 19:11:12 -0600

    new file:   Module-1/Exercice02/index.html 	new file:   Module-1/Exercice02/js/code.js 	new file:   Module-1/Exercise01/exercice01.js 	new file:   Module-1/Exercise03/index.html 	new file:   Module-1/Exercise04/atomic-datatypes.js 	new file:   Module-1/Exercise04/temporal-dead-zone.js

---
+++ Module-1/Exercise03/index.js
@@ -0,0 +1,8 @@
+let firstValue = Number.parseInt(prompt('Enter the first value'));
+let secondValue = Number.parseInt(prompt('Enter the second value'));
+
+debugger;
+let result = firstValue + secondValue;
+
+console.log(result);
+alert(result);
